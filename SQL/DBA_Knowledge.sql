918b234e4a7ca85d633bf7887fccbdb1f4f52643
Rhyan Skipworth <70857572+Rhyanskipworth@users.noreply.github.com>
2020-11-13 19:45:36 -0800

    Update DBA_Knowledge.sql

--- SQL/DBA_Knowledge.sql
+++ SQL/DBA_Knowledge.sql
@@ -20,50 +20,6 @@
 	
 		/* To verify dbss name */
 		   SELECT * FROM sys.databases   [or]   SELECT * FROM sysdatabases
-		   
-		   
-
--- Creates a stored procedure to view each sales rep's overall revenue ordered by country.
-
-	USE [Northwind]
-	GO
-
-	ALTER PROCEDURE [dbo].[CustomerOrderByCountry]
-
-	@Country varchar(15),
-	@DateFrom date,
-	@DateTo date
-
-	AS
-
-	/*
-	11/10/2020	RS	created inital procedure
-	11/11/2020	RS	Added date parameters, handled nulls, and changed data type for o.OrderDate
-	*/
-
-	SELECT
-		CONCAT(e.FirstName, ' ', e.LastName) Employee,
-		c.ContactName Customer,
-		SUM(o.Freight) OrderAmt,
-		o.ShipCountry,
-		CAST(o.OrderDate as date) OrderDate			-- Converted o.OrderDate from DateTime to Date
-	FROM Orders o
-		INNER JOIN Customers c
-			ON o.CustomerID = c.CustomerID
-		INNER JOIN Employees e
-			ON e.EmployeeID = o.EmployeeID
-	WHERE (o.ShipCountry = @Country					-- Set parameter = NULL to allow end-user input and retrieve all dates as default.
-			OR @Country IS NULL)
-	 	AND (o.OrderDate BETWEEN @DateFrom AND @DateTo
-				OR @DateFrom IS NULL
-					OR @DateTo IS NULL)
-	GROUP BY 
-		e.FirstName, 
-		e.LastName, 
-		c.ContactName, 
-		o.ShipCountry,
-		O.OrderDate
-	ORDER BY OrderAmt DESC
 
 
 
