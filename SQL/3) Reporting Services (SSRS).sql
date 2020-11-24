63b9a0e1092ceb7958ea3f6e351a40b6214301c5
Rhyan Skipworth <70857572+Rhyanskipworth@users.noreply.github.com>
2020-11-23 16:32:49 -0800

    Update 3) Reporting Services (SSRS).sql

--- SQL/3) Reporting Services (SSRS).sql
+++ SQL/3) Reporting Services (SSRS).sql
@@ -1,3 +1,3 @@
 Web Portal      http://desktop-uid1jce/Reports/browse/
-Administrator 	DESKTOP-UID1JCE\
+Administrator 	DESKTOP-UID1JCE\                         -- Allows access to user on a seperate device.
 
