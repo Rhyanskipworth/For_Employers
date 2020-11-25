d2891fe6830150bf2b24a970f41c6df107bce846
Rhyan Skipworth <70857572+Rhyanskipworth@users.noreply.github.com>
2020-11-25 09:24:28 -0800

    Update [3] SSRS.sql

--- T-SQL/[3] SSRS.sql
+++ T-SQL/[3] SSRS.sql
@@ -1,5 +1,4 @@
 View my SSRS Web Portal  @  http://desktop-uid1jce/Reports/browse/
-                            http://desktop-uid1jce/ReportServer
 
 User/Administrator:	DESKTOP-UID1JCE\    -- Allows user access to edit reports on a seperate remote machine.
 
