8c11302cce3e515a8209bb427cea78d7a55bcff2
Rhyan Skipworth <70857572+Rhyanskipworth@users.noreply.github.com>
2020-12-05 19:41:12 -0800

    Update [1] DBA.sql

--- T-SQL/[1] DBA.sql
+++ T-SQL/[1] DBA.sql
@@ -1,4 +1,4 @@
-PowerBI: As a general rule, use measures whenever you can (for numbers) and you only used calcolumns if you need the physical structure of the column (to slice and dice).
+--PowerBI: As a general rule, use measures whenever you can (for numbers) and you only used calcolumns if you need the physical structure of the column (to slice and dice).
 
 /* Data migration techniques,
 	1st option: Back up the source data files then restore '.bak' files onto the destination server DB.
